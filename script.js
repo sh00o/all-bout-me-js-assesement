dc816312326c1ffc27639940af7ffe0ddbc13dfb
sh00o <j3553jr@outlook.com>
2026-03-29 10:11:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const back_card = document.getElementById("back_card");
-const middle_card = document.getElementById("middle_card");
 back_card.onclick = () => {
   back_card.classList.toggle("move-front-cards");
-  middle_card.classList.toggle("middle-card");
 };
