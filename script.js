98e2f21bbb3a08fc2ca7c8e28613101a037e4092
sh00o <j3553jr@outlook.com>
2026-03-30 20:29:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const back_card = document.getElementById("back_card");
+const middle_card = document.getElementById("middle_card");
+const right_card = document.getElementById("right_card");
 back_card.onclick = () => {
   back_card.classList.toggle("active");
+};
+middle_card.onclick = () => {
+  middle_card.classList.toggle("active");
+};
+right_card.onclick = () => {
+  right_card.classList.toggle("active");
 };
