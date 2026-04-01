19a47dd1d9c9cd924d8035376ddd11633f86b588
sh00o <j3553jr@outlook.com>
2026-04-01 16:26:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const middle_card = document.getElementById("middle_card");
 const front_card = document.getElementById("front_card");
 back_card.onclick = () => {
   back_card.classList.toggle("active");
-  middle_card.classList.toggle("active");
-  front_card.classList.toggle("active");
+  middle_card.classList.toggle("move");
+  front_card.classList.toggle("move");
 };
 middle_card.onclick = () => {
-  front_card.classList.toggle("active");
+  front_card.classList.toggle("move");
 };
 front_card.onclick = () => {
   front_card.classList.toggle("active");
